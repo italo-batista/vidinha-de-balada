b64a783b4952e2fb2d3ca070b35629ff80a8e690
Leticia Wanderley <leticiafwanderley@gmail.com>
2017-06-10 17:12:32 -0300

    Grafico de linhas nao plotado

--- js/directives/lineChart.js
+++ js/directives/lineChart.js
@@ -18,7 +18,7 @@ app.directive('lineChart', function ($parse, $window) {
           var height = height - margin.top - margin.bottom;
           var g = chart.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-        var parseTime = d3.timeParse("%Y%m");
+        var parseTime = d3.timeParse("%m/%Y");
 
         var x = d3.scaleTime().range([0, width]),
             y = d3.scaleLinear().range([height, 0]),
@@ -27,15 +27,13 @@ app.directive('lineChart', function ($parse, $window) {
         var line = d3.line()
             .curve(d3.curveBasis)
             .x(function(d) { return x(d.date); })
-            .y(function(d) { return y(d.gasto); });
+            .y(function(d) { return y(d.total); });
 
         d3.csv("data/gasto_mensal_por_depoutado_por_categoria.csv", type, function(error, data) {
           if (error) throw error;
 
           var gastoTotal = d3.nest()
-            .key(function(d) {return d.txNomeParlamentar;})
-            .key(function(d) {return d.ano;})
-            .key(function(d) {return d.mes;})
+            .key(function(d) {return (d.txNomeParlamentar + " " + d.mes + "/" + d.ano);})
             .rollup(function(d) {
               return d3.sum(d, function(g) {return +g.total; });
             }).entries(data);
@@ -43,48 +41,54 @@ app.directive('lineChart', function ($parse, $window) {
              d.date = d.key;
              d.value = d.values;
             });
-          console.log("$$$$$$", gastoTotal);
-        //   x.domain(d3.extent(data, function(d) { return d.date; }));
-        //
-        //   y.domain([
-        //     d3.min(cities, function(c) { return d3.min(c.values, function(d) { return d.temperature; }); }),
-        //     d3.max(cities, function(c) { return d3.max(c.values, function(d) { return d.temperature; }); })
-        //   ]);
-        //
-        //   z.domain(cities.map(function(c) { return c.id; }));
-        //
-        //   g.append("g")
-        //       .attr("class", "axis axis--x")
-        //       .attr("transform", "translate(0," + height + ")")
-        //       .call(d3.axisBottom(x));
-        //
-        //   g.append("g")
-        //       .attr("class", "axis axis--y")
-        //       .call(d3.axisLeft(y))
-        //     .append("text")
-        //       .attr("transform", "rotate(-90)")
-        //       .attr("y", 6)
-        //       .attr("dy", "0.71em")
-        //       .attr("fill", "#000")
-        //       .text("Temperature, ºF");
-        //
-        //   var city = g.selectAll(".city")
-        //     .data(cities)
-        //     .enter().append("g")
-        //       .attr("class", "city");
-        //
-        //   city.append("path")
-        //       .attr("class", "line")
-        //       .attr("d", function(d) { return line(d.values); })
-        //       .style("stroke", function(d) { return z(d.id); });
-        //
-        //   city.append("text")
-        //       .datum(function(d) { return {id: d.id, value: d.values[d.values.length - 1]}; })
-        //       .attr("transform", function(d) { return "translate(" + x(d.value.date) + "," + y(d.value.temperature) + ")"; })
-        //       .attr("x", 3)
-        //       .attr("dy", "0.35em")
-        //       .style("font", "10px sans-serif")
-        //       .text(function(d) { return d.id; });
+
+            gastoTotal.forEach(function(d) {
+             d.date = d.key.split(" ").slice(-1)[0];
+             d.nome = d.key.replace(d.date, "").trim();
+             d.total = d.value;
+            });
+          console.log('%%%%%%%', gastoTotal)
+          x.domain(d3.extent(gastoTotal, function(d) { return d.date; }));
+
+          y.domain([
+            d3.min(gastoTotal, function(c) { return c.total; }),
+            d3.max(gastoTotal, function(c) { return c.total; })
+          ]);
+
+          z.domain(gastoTotal.map(function(c) { return c.nome; }));
+          console.log('&&&&&&&&&');
+          g.append("g")
+              .attr("class", "axis axis--x")
+              .attr("transform", "translate(0," + height + ")")
+              .call(d3.axisBottom(x));
+          console.log('%%%%%%%%%%%%');
+          g.append("g")
+              .attr("class", "axis axis--y")
+              .call(d3.axisLeft(y))
+            .append("text")
+              .attr("transform", "rotate(-90)")
+              .attr("y", 6)
+              .attr("dy", "0.71em")
+              .attr("fill", "#000")
+              .text("Gasto total, R$");
+          console.log('***********')
+          var city = g.selectAll(".city")
+            .data(gastoTotal)
+            .enter().append("g")
+              .attr("class", "city");
+
+          city.append("path")
+              .attr("class", "line")
+              .attr("d", function(d) { return line({"total": d.total, "date":d.date}); })
+              .style("stroke", function(d) { return z(d.nome); });
+
+          city.append("text")
+              .datum(function(d) { return {nome: d.nome, total: d.total}; })
+              .attr("transform", function(d) { return "translate(" + x(d.date) + "," + y(d.total) + ")"; })
+              .attr("x", 3)
+              .attr("dy", "0.35em")
+              .style("font", "10px sans-serif")
+              .text(function(d) { return d.nome; });
         });
 
         function type(d, _, columns) {
