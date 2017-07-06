8d8c3be480960e2f7d8a0cc9561ab12a8f50c950
Jefferson Neves <jefferson.rpn@gmail.com>
2017-07-06 17:44:16 -0300

    WIP: Adicionando textos de equivalentes ao gastômetro

---
+++ web/app/scripts/directives/textCarousel.drct.js
@@ -0,0 +1,58 @@
+(function() {
+    'use strict';
+
+    angular.module('baladaApp')
+        .directive('vbTextCarousel', ['$timeout', vbTextCarousel]);
+
+
+    /*jshint latedef: nofunc */
+    function vbTextCarousel($timeout) {
+        return {
+            restrict: 'A',
+            scope: {
+              carouselItems: '=',
+              interval: '='
+            },
+            link: function (scope, elem, attrs) {
+              var timeoutId, index = 0;
+
+              function setCarouselText() {
+                if (scope.carouselItems.length > 0) {
+                  elem.html('<strong>'+scope.carouselItems[index].valor+'<strong> '+scope.carouselItems[index].texto);
+                }
+              }
+
+              function goToNextValue() {
+                index += 1;
+                if (index >= scope.carouselItems.length) {
+                  index = 0;
+                }
+              };
+
+              function scheduleNext() {
+                timeoutId = $timeout(function () {
+                  elem.fadeOut(scope.interval/10, function () {
+                    $(this).html('<strong>'+scope.carouselItems[index].valor+'<strong> '+scope.carouselItems[index].texto).fadeIn(scope.interval/10);
+                    updateCarousel();
+                  });
+                }, scope.interval);
+              };
+
+              function updateCarousel() {
+                setCarouselText();
+                goToNextValue();
+                scheduleNext();
+              };
+
+              updateCarousel();
+
+              elem.on('$destroy', function () {
+                $timeout.cancel(timeoutId);
+            });
+          }
+        };
+    }
+
+
+
+})();
