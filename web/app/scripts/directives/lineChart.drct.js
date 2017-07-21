ca4d203d9196bcdab8e178d823715ce2a63b1847
Leticia Wanderley <leticiafwanderley@gmail.com>
2017-07-20 22:27:58 -0300

    Tooltip de gastos

--- web/app/scripts/directives/lineChart.drct.js
+++ web/app/scripts/directives/lineChart.drct.js
@@ -22,18 +22,24 @@ app.directive('lineChart', function ($parse, RESTAPI) {
       var g = chart.append("g")
         .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
+      var parseTime = d3.timeParse("%m/%Y");
+      var formatTime = d3.timeFormat("%m/%Y");
+
       var x = d3.scaleTime().range([0, width]),
           y = d3.scaleLinear().range([height, 0]);
 
       var line = d3.line()
-        .curve(d3.curveBasis)
+        .curve(d3.curveLinear)
         .x(function (d) { return x(d.date); })
-        .y(function (d) { return y(+d.total); });
+        .y(function (d) { return y(+d.valor); });
+
+      var gasto = d3.select("body").append("div")
+        .attr("class", "tooltip")
+        .style("opacity", 0);
 
       d3.json(RESTAPI+"timeline?id="+scope.deputado, function (error, data) {
         if (error) throw error;
 
-        var parseTime = d3.timeParse("%m/%Y");
         data.forEach(function (d) {
           d.date = parseTime(Object.keys(d)[0]);
           d.valor = +d[Object.keys(d)[0]]
@@ -52,10 +58,6 @@ app.directive('lineChart', function ($parse, RESTAPI) {
           })
         ]);
 
-        var line = d3.line()
-          .x(function(d) { return x(d.date); })
-          .y(function(d) { return y(d.valor); });
-
         g.append("path")
           .datum(data)
           .attr("fill", "None")
@@ -63,17 +65,40 @@ app.directive('lineChart', function ($parse, RESTAPI) {
           .attr("stroke-linejoin", "round")
           .attr("stroke-linecap", "round")
           .attr("stroke-width", 2)
-          .attr("d", line);
+          .attr("d", line)
+
+        g.selectAll("point")
+          .data(data)
+          .enter().append("circle")
+          .attr("r", 3)
+          .attr("cx", function(d) { return x(d.date); })
+          .attr("cy", function(d) { return y(+d.valor); })
+          .attr("fill", "transparent")
+          .on("mouseover", function(d) {
+             gasto.transition()
+               .duration(200)
+               .style("opacity", 1);
+             gasto.html(formatTime(d.date) + "<br/>R$ " + d.valor)
+               .style("left", (d3.event.pageX) + "px")
+               .style("top", (d3.event.pageY - 30) + "px");
+             })
+           .on("mouseout", function(d) {
+             gasto.transition()
+               .duration(500)
+               .style("opacity", 0);
+             });
 
         g.append("g")
           .attr("class", "axis axis--x")
           .attr("transform", "translate(0," + height + ")")
-          .call(d3.axisBottom(x).tickFormat(d3.timeFormat("%m/%Y")));
+          .call(d3.axisBottom(x).tickFormat(d3.timeFormat("%m/%Y")))
+          .style("font-size", "16px");
 
         g.append("g")
           .attr("class", "axis axis--y")
           .style("font", "14px sans-serif")
-          .call(d3.axisLeft(y))
+          .call(d3.axisLeft(y).tickSize(0))
+          .style("font-size", "16px")
           .append("text")
           .attr("transform", "rotate(-90)")
           .attr("y", 6)
