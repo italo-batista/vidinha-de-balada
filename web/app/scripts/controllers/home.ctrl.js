f8c03409789944e235d5ecbf89cb7562a786d384
Helder Ronyer <helderronyer@gmail.com>
2017-07-29 13:56:25 -0300

    fixa menu no topo ao rolar barra

--- web/app/scripts/controllers/home.ctrl.js
+++ web/app/scripts/controllers/home.ctrl.js
@@ -84,5 +84,20 @@
 
             init();
 
+            $(document).ready(function () {
+
+            var menu = $('.menu');
+            var origOffsetY = menu.offset().top;
+
+            function scroll() {
+                if ($(window).scrollTop() >= origOffsetY) {
+                    $('.menu').addClass('navbar-fixed-top');
+                } else {
+                    $('.menu').removeClass('navbar-fixed-top');
+                }
+            }
+            document.onscroll = scroll;
+});
+
         });
 })();
