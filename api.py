ef37234473e0c5535fbd135b0b4bb7a6d9b364a6
Michelly Menezes <martha.michelly@gmail.com>
2017-07-29 13:48:35 -0300

    adiciona busca de ids de deputados(as) por nome

--- api.py
+++ api.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python
 # coding: utf-8
-import ConfigParser  
+import ConfigParser
 import sqlalchemy
 import datetime
 import sys
-from flask.ext.sqlalchemy import SQLAlchemy  
+from unidecode import unidecode
+from flask.ext.sqlalchemy import SQLAlchemy
 from flask import Flask, jsonify, request
 
-# needed to install: 
+# needed to install:
 # 	sudo apt-get install python-mysqldb
-#	sudo pip install mysql-python 
+#	sudo pip install mysql-python
 
 # use this material:
 # http://blog.cloudoki.com/getting-started-with-restful-apis-using-the-flask-microframework-for-python/
@@ -25,7 +26,7 @@ from flask import Flask, jsonify, request
 app = Flask(__name__)
 
 # MySQL configurations
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:Pesquisas@localhost/vidinha_balada?charset=utf8'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:martha@localhost/vidinha_balada?charset=utf8'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
 mysql = SQLAlchemy(app)
@@ -53,7 +54,7 @@ if now.month == 1:
 else:
 	mesPassado = now.month - 1
 	ano = now.year
-	
+
 categoria_alimentacao = 'Alimentação'
 categoria_escritorio = 'Escritório'
 categoria_divulgacao = 'Divulgação de atividade parlamentar'
@@ -67,27 +68,27 @@ categoria_passagens = 'Passagens aéreas'
 #categoria_locacao = 'Locação de'
 #categoria_combustivel = 'Combustíve'
 #categoria_passagens = 'Passagens'
-	
+
 def init():
 	pass
 
 # Models --------------------------------------------------------------
 
-class Cota(mysql.Model):  
+class Cota(mysql.Model):
     __tablename__ = 'cotas'
     uf = mysql.Column(mysql.String(2), primary_key=True)
     cota = mysql.Column(mysql.Integer, nullable=False)
-    
+
     def __init__(self, uf, cota):
         self.uf = uf
         self.cota = cota
 
     def __repr__(self):
         return '<Cotas (%s, %s) >' % (self.uf, self.cota)
-   
-class Deputado(mysql.Model):  
+
+class Deputado(mysql.Model):
     __tablename__ = 'deputado'
-    
+
     id = mysql.Column(mysql.String(7), primary_key=True)
     nome = mysql.Column(mysql.String(15))
     partidoAtual = mysql.Column(mysql.String(20))
@@ -96,13 +97,13 @@ class Deputado(mysql.Model):
     twitter = mysql.Column(mysql.String(10))
     telefone = mysql.Column(mysql.String(15))
     email = mysql.Column(mysql.String(20))
-    
+
     def __repr__(self):
         return '<Deputado (%s, %s, %s, %s, %s, %s, %s, %s) >' % (self.id, self.nome, self.partidoAtual, self.uf, self.foto, self.twitter, self.telefone, self.email)
 
-class Gasto(mysql.Model):  
+class Gasto(mysql.Model):
     __tablename__ = 'gastos'
-    
+
     id = mysql.Column(mysql.String(10), primary_key=True)
     idDeputado = mysql.Column(mysql.String(7), nullable=False)
     mesEmissao = mysql.Column(mysql.Integer)
@@ -111,59 +112,59 @@ class Gasto(mysql.Model):
     nomeFornecedor = mysql.Column(mysql.String(15), nullable=False)
     valor = mysql.Column(mysql.Float)
     cnpj = mysql.Column(mysql.String(15))
-    
+
     def __repr__(self):
         return '<Gasto (%s, %s, %s, %s, %s, %s, %s) >' % (self.idDeputado, self.mesEmissao, self.anoEmissao, self.idCategoria, self.nomeFornecedor, self.valor, self.cnpj)
 
-class SessoesMes(mysql.Model):  
+class SessoesMes(mysql.Model):
     __tablename__ = 'sessoesMes'
-    
+
     mes = mysql.Column(mysql.Integer, primary_key=True)
     ano = mysql.Column(mysql.Integer, primary_key=True)
     quantidadeSessoes = mysql.Column(mysql.Integer)
-    
+
     def __repr__(self):
         return '<SessoesMes (%s, %s, %s) >' % (self.mes, self.ano, self.quantidadeSessoes)
 
-class SessoesMesDeputado(mysql.Model):  
+class SessoesMesDeputado(mysql.Model):
     __tablename__ = 'sessoesMesDeputado'
-    
+
     mes = mysql.Column(mysql.Integer, primary_key=True)
     ano = mysql.Column(mysql.Integer, primary_key=True)
     idDeputado = mysql.Column(mysql.String(7), primary_key=True)
     quantidadeParticipacoes = mysql.Column(mysql.Integer)
-    
+
     def __repr__(self):
         return '<SessoesMesDeputado (%s, %s, %s, %s) >' % (self.mes, self.ano, self.idDeputado, self.quantidadeParticipacoes)
 
-class EmendasPropostasDeputado(mysql.Model):  
+class EmendasPropostasDeputado(mysql.Model):
     __tablename__ = 'emendasPropostasDeputado'
-    
+
     mes = mysql.Column(mysql.Integer, primary_key=True)
     ano = mysql.Column(mysql.Integer, primary_key=True)
     idDeputado = mysql.Column(mysql.String(7), primary_key=True)
     quantidade = mysql.Column(mysql.Integer)
-    
+
     def __repr__(self):
         return '<EmendasPropostasDeputado (%s, %s, %s, %s) >' % (self.mes, self.ano, self.idDeputado, self.quantidade)
 
-class SelosDeputado(mysql.Model):  
+class SelosDeputado(mysql.Model):
     __tablename__ = 'selosDeputado'
 
-    idDeputado = mysql.Column(mysql.String(7), primary_key=True)    
+    idDeputado = mysql.Column(mysql.String(7), primary_key=True)
     mes = mysql.Column(mysql.Integer, primary_key=True)
     ano = mysql.Column(mysql.Integer, primary_key=True)
     idCategoria = mysql.Column(mysql.String(10), primary_key=True)
-    
+
     def __repr__(self):
         return '<SelosDeputado (%s, %s, %s, %s) >' % (self.idDeputado, self.mes, self.ano, self.idCategoria)
 
-class Empresa(mysql.Model):  
+class Empresa(mysql.Model):
     __tablename__ = 'empresas'
 
-    cnpj = mysql.Column(mysql.String(15), primary_key=True)    
+    cnpj = mysql.Column(mysql.String(15), primary_key=True)
     nome = mysql.Column(mysql.String(10))
-    
+
     def __repr__(self):
         return '<Empresa (%s, %s) >' % (self.cnpj, self.nome)
 
@@ -171,15 +172,15 @@ class Empresa(mysql.Model):
 # Routes --------------------------------------------------------------
 
 @app.route("/")
-def hello():  
+def hello():
     return "Hello World!"
 
 @app.route('/gasto_anual')
 def getGasto(ano):
 	pass
-    
+
 @app.route('/cotas', methods=['GET'])
-def getCotas():  
+def getCotas():
     data = Cota.query.all()
 
     data_all = []
@@ -188,23 +189,35 @@ def getCotas():
         data_all.append([cota.uf, cota.cota])
 
     return jsonify(cotas=data_all)
-    
+
 @app.route('/cota/<uf>', methods=['GET'])
 def getCota(uf):
 	cota = Cota.query.filter_by(uf=uf).first_or_404()
 	data_all = [cota.uf, cota.cota]
 	return jsonify(cotas=data_all)
 
+@app.route('/buscaDeputado/<nome>', methods=['GET'])
+def getEmpresasDeputado(nome):
+	data = Deputado.query.all()
+
+	data_all = []
+
+	for deputado in data:
+		if(unidecode(nome.upper()) in unidecode(deputado.nome.upper())):
+			data_all.append(deputado.id)
+
+	return jsonify(buscaDeputado=data_all)
+
 def somaGastosTotais(query_gasto_categoria):
 	gastoTotal = 0
 	for gasto in query_gasto_categoria:
 		gastoTotal = gastoTotal + gasto.valor
 	return gastoTotal
-	
+
 @app.route('/deputados/<id>', methods=['GET'])
 def getDeputado(id):
-			
-	deputado = Deputado.query.filter_by(id=id).first()	
+
+	deputado = Deputado.query.filter_by(id=id).first()
 	#query_gasto_alimentacao = Gasto.query.filter_by(idDeputado=id, mesEmissao=mesPassado, anoEmissao=ano, nomeCategoria=categoria_alimentacao).all()
 	#query_gasto_escritorio = Gasto.query.filter_by(idDeputado=id, mesEmissao=mesPassado, anoEmissao=ano, nomeCategoria=categoria_escritorio).all()
 	#query_gasto_divulgacao = Gasto.query.filter_by(idDeputado=id, mesEmissao=mesPassado, anoEmissao=ano, nomeCategoria=categoria_divulgacao).all()
@@ -219,19 +232,19 @@ def getDeputado(id):
 	query_gasto_passagens = Gasto.query.filter_by(idDeputado=id, nomeCategoria=categoria_passagens).all()
 	presencas_deputado = SessoesMesDeputado.query.filter_by(idDeputado=id, mes=mesPassado, ano=ano).first() # sessoes so do mes passado
 	presencas_total = SessoesMes.query.filter_by(mes=mesPassado, ano=ano).first() # sessoes so do mes passado
-		
+
 	gasto_alimentacao = somaGastosTotais(query_gasto_alimentacao)
 	gasto_escritorio = somaGastosTotais(query_gasto_escritorio)
 	gasto_divulgacao = somaGastosTotais(query_gasto_divulgacao)
 	gasto_locacao = somaGastosTotais(query_gasto_locacao)
 	gasto_combustivel = somaGastosTotais(query_gasto_combustivel)
 	gasto_passagens = somaGastosTotais(query_gasto_passagens)
-		
+
 	## o total dos gastos é a soma dos gastos das categorias anteriores ou envolvem outros gastos?
 	total_gastos = gasto_alimentacao + gasto_escritorio + gasto_divulgacao + gasto_locacao + gasto_combustivel + gasto_passagens
 	presencas = presencas_deputado.quantidadeParticipacoes if (presencas_deputado != None) else 0
 	total_sessoes = presencas_total.quantidadeSessoes if (presencas_total != None) else 0
-		
+
 	json = {
 	'Nome' : deputado.nome.decode("utf-8"),
 	'urlfoto' : deputado.foto,
@@ -245,8 +258,8 @@ def getDeputado(id):
 	'total_sessoes': total_sessoes,
 	'Total' : total_gastos
 	}
-	
-	return jsonify(json)    
 
-if __name__ == "__main__":  
+	return jsonify(json)
+
+if __name__ == "__main__":
     app.run(debug=True)
